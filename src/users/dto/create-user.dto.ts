a10294099d82ae5212d2c53f6adfd4f01fb27c18
chrisbohne <christoph.bohne90@gmail.com>
2021-11-02 16:56:29 +0100

    add basic validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,13 +1,18 @@
-import { IsNotEmpty, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   id: number;
+
   @IsNotEmpty()
   @MinLength(3)
   username: string;
+
+  @IsEmail({}, { message: 'Please enter a valid email' })
   email: string;
+
   @IsNotEmpty()
   @MinLength(8)
   password: string;
+
   experience: number;
 }
