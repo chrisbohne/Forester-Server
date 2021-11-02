fcc688f1ccf95ebe3259e2bcb4233a50e139edd7
chrisbohne <christoph.bohne90@gmail.com>
2021-11-02 15:29:09 +0100

    connect to database and create migration and seed db

---
+++ data.ts
@@ -0,0 +1,29 @@
+export const users = [
+  {
+    username: 'Ruso',
+    email: 'Ruso@gmail.com',
+    password: '1234',
+    experience: 50,
+  },
+  {
+    username: 'Jelly',
+    email: 'Jelly@gmail.com',
+    password: '1234',
+    experience: 100,
+  },
+];
+
+export const trees = [
+  {
+    name: 'Amazing Tree',
+    long: 50.564,
+    lat: 40.255,
+    photo: 'default.jpg',
+  },
+  {
+    name: 'Good Tree',
+    long: 50.564,
+    lat: 40.255,
+    photo: 'default.jpg',
+  },
+];
---
+++ prisma/migrations/20211102141539_init/migration.sql
@@ -0,0 +1,47 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "experience" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Tree" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "long" DOUBLE PRECISION NOT NULL,
+    "lat" DOUBLE PRECISION NOT NULL,
+    "photo" TEXT NOT NULL,
+
+    CONSTRAINT "Tree_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Hug" (
+    "id" SERIAL NOT NULL,
+    "message" TEXT,
+    "experience" INTEGER NOT NULL,
+    "duration" DOUBLE PRECISION NOT NULL,
+    "huggedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+    "treeId" INTEGER NOT NULL,
+
+    CONSTRAINT "Hug_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Hug" ADD CONSTRAINT "Hug_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Hug" ADD CONSTRAINT "Hug_treeId_fkey" FOREIGN KEY ("treeId") REFERENCES "Tree"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
