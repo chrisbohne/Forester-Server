4680c44963b22ad80b3e4ac373820bcb3b2c78f9
chrisbohne <christoph.bohne90@gmail.com>
2021-11-04 10:46:02 +0100

    add Hug service and controller

--- src/hugs/entities/hug.entity.ts
+++ src/hugs/entities/hug.entity.ts
@@ -1,1 +1,15 @@
-export class Hug {}
+import { Hug } from '.prisma/client';
+
+export class HugEntity implements Hug {
+  id: number;
+  message: string;
+  huggedAt: Date;
+  duration: number;
+  experience: number;
+  treeId: number;
+  userId: number;
+
+  constructor(partial: Partial<HugEntity>) {
+    Object.assign(this, partial);
+  }
+}
